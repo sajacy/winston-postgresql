054d070c14be13213a919d1d9da483aff7033c52
ofkindness <andrei.tretyakov@gmail.com>
2017-08-06 16:30:42 +0300

    - Add winston test

--- test/winston-pg-native-test.js
+++ test/winston-pg-native-test.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @module 'winston-pg-native-test'
+ * @module 'winston-pg-transport-test'
  * @fileoverview Tests of winston transport for logging into PostgreSQL
  * @license MIT
  * @author Andrei Tretyakov <andrei.tretyakov@gmail.com>
@@ -8,41 +8,18 @@
 
 var vows = require('vows');
 var transport = require('winston/test/transports/transport');
-var Postgres = require('../lib/winston-pg-native');
-// var Promise = require('bluebird');
-// var winston = require('winston');
-// var pg = require('pg').native;
+var Postgres = require('../lib/winston-pg-transport.js');
 
-var conString = "postgres://" + process.env.POSTGRES_USER +
-  ":" + process.env.POSTGRES_PASSWORD +
-  "@" + process.env.POSTGRES_HOST +
-  ":" + process.env.POSTGRES_PORT +
-  "/" + process.env.POSTGRES_DBNAME;
+var conString = 'postgres://' + process.env.POSTGRES_USER +
+':' + process.env.POSTGRES_PASSWORD +
+'@' + process.env.POSTGRES_HOST +
+':' + process.env.POSTGRES_PORT +
+'/' + process.env.POSTGRES_DBNAME;
 
-vows.describe('winston-pg-native')
+vows.describe('winston-pg-transport')
   .addBatch({
     'An instance of the Postgres Transport': transport(Postgres, {
-      conString: conString,
-      tableConfig: {
-        tableName: 'winston_logs',
-        tableFields: 'level, msg, meta'
-      },
-      level: 'info'
+      conString: conString
     })
   })
-  // .addBatch({
-  // 'An instance of the Postgres Transport with promise':
-  //     transport(Postgres, {
-  //       conString: new Promise(function(resolve, reject) {
-  //         pg.connect(conString,
-  //             function(err, db) {
-  //               if (err) {
-  //                 reject(err);
-  //               } else {
-  //                 resolve(db);
-  //               }
-  //             });
-  //       })
-  //     })
-  // })
   .export(module);
